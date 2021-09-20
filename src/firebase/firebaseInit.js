737ec97b288ddfb7d0b6a2fa5434da07db114323
ardhikarn <ardhikarn@gmail.com>
2021-09-20 13:54:32 +0700

    first commit

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore"
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCMgKVfiU0MiIFoL8EkfPqNszR7IFlikyQ",
+  authDomain: "fireblogs-d96df.firebaseapp.com",
+  projectId: "fireblogs-d96df",
+  storageBucket: "fireblogs-d96df.appspot.com",
+  messagingSenderId: "1044636066170",
+  appId: "1:1044636066170:web:21d0e1710c7b16a36f36c3"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { timestamp }
+export default firebaseApp.firestore
