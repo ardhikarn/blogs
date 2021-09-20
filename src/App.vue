e6dda9ed078657077357df9cf73fe1bc0947ebfe
ardhikarn <ardhikarn@gmail.com>
2021-09-20 14:54:18 +0700

    footer component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="app-wrapper">
     <div class="app">
-      <Navigation/>
+      <Navigation />
       <router-view />
+      <Footer />
     </div>
   </div>
 </template>
 
 <script>
-import Navigation from './components/Navigation.vue'
+import Navigation from "./components/Navigation.vue";
+import Footer from "./components/Footer.vue";
 export default {
   name: "app",
-  components: {Navigation},
+  components: { Navigation, Footer },
   data() {
     return {};
   },
