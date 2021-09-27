3678902724f736d52e4fe429f5bd8605a6aa47b7
ardhikarn <ardhikarn@gmail.com>
2021-09-27 08:17:05 +0700

    forgot password page

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,12 +1,51 @@
 <template>
-  <div></div>
+  <div class="reset-password">
+    <div class="form-wrap">
+      <form class="reset">
+        <h2>Reset Password</h2>
+        <p>Forgot password? Enter your email to reset it</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email" />
+            <Email class="icon" />
+          </div>
+        </div>
+        <button>Reset</button>
+        <div class="angle"></div>
+      </form>
+      <div class="background"></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Email from "../assets/Icons/envelope-regular.svg";
 export default {
   name: "ForgotPassword",
+  components: {
+    Email,
+  },
+  data() {
+    return {
+      email: null,
+    };
+  },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.reset-password {
+  position: relative;
+  .form-wrap {
+    .reset {
+      h2 {
+        margin-bottom: 8px;
+      }
+      p {
+        margin-bottom: 32px;
+        text-align: center;
+      }
+    }
+  }
+}
 </style>
